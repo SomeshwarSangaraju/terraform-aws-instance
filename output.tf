f56e3d0d601b019595f414a66ced618afc85dfea
SSR <someshwar.sangaraju19@gmail.com>
2025-10-27 19:49:09 +0530

    terraform module

---
+++ output.tf
@@ -0,0 +1,12 @@
+output "public_ip"{
+    value="aws_instance.this.public_ip"
+}
+
+output  "private_ip" {
+  value       = aws_instance.this.private_ip
+  description = "private IP of the instance created"
+}
+
+output "instance_id" {
+  value = aws_instance.this.id
+}

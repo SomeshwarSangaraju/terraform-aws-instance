1a8d0026f32e052735ba6a660e77021bbfa65e91
SSR <someshwar.sangaraju19@gmail.com>
2025-10-27 19:46:19 +0530

    terrform modules

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "ami_id"{
+    type="string"
+    default="ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+  type = string
+  description = "Instance type used for creating EC2 instance"
+  validation {
+    condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+    error_message = "Please select either t3 micro or small or medium"
+  }
+}
+variable "sg_ids"{
+    type=list
+}
+
+variable "tags"{
+    type=map
+    default={}
+}
